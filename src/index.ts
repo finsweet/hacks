e22d3082589d521daf179a849a9fab8cc4ba9aa6
Brian Musuku <brayanfinsweet@github.com>
2022-11-21 11:21:19 +0300

    feat: add copy component button

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,47 @@ import { formatCode, appendHeadScript } from '$utils/domUtils';
 window.Webflow ||= [];
 window.Webflow.push(async () => {
   const codeListings = document.querySelectorAll<HTMLDivElement>(SELECTORS.CODE_LISTING);
+  const componentCopyButtons = document.querySelectorAll<HTMLAnchorElement>(
+    `[${SELECTORS.COPY_COMPONENT}]`
+  );
+
+  componentCopyButtons.forEach(async (componentCopyButton) => {
+    componentCopyButton.addEventListener('click', async () => {
+      const url = componentCopyButton.getAttribute(SELECTORS.COPY_COMPONENT);
+      if (!url) return;
+
+      const resp = await fetch(url);
+      const wfJson = await resp.json();
+
+      try {
+        document.addEventListener('copy', (e) => {
+          copyButtonClicked(e, wfJson);
+        });
+        document.execCommand('copy');
+      } catch (e) {
+      } finally {
+        document.removeEventListener('copy', (e) => {
+          copyButtonClicked(e, wfJson);
+        });
+      }
+
+      const textDiv = componentCopyButton.lastChild as HTMLDivElement;
+      const previousText = textDiv.innerText;
+      textDiv.innerText = 'Copied!';
+
+      setTimeout(() => {
+        textDiv.innerText = previousText;
+      }, 2000);
+    });
+  });
 
   await Promise.all(
     [...codeListings].map(async (codeListing) => {
       const url = codeListing.getAttribute('url');
-      const codeType = codeListing.getAttribute('code-type');
       const copyTrigger = codeListing.parentElement?.querySelector<HTMLAnchorElement>(
         '[fs-copyclip-element="click"]'
       );
-      if (!url || !codeType || !copyTrigger) return;
+      if (!url || !copyTrigger) return;
 
       const resp = await fetch(url);
       const code = await resp.text();
@@ -39,3 +71,8 @@ window.Webflow.push(async () => {
 
   appendHeadScript(SELECTORS.CODE_HIGHLIGHT_ATTR_URL);
 });
+
+const copyButtonClicked = (event: ClipboardEvent, wfJson: JSON) => {
+  event.clipboardData?.setData('application/json', JSON.stringify(wfJson));
+  event.preventDefault();
+};
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,5 @@ export const SELECTORS = {
   CODE_HIGHLIGHT_ATTR_URL:
     'https://cdn.jsdelivr.net/npm/@finsweet/attributes-codehighlight@1/codehighlight.js',
   CODE_LISTING: '[fs-codehighlight-element]',
+  COPY_COMPONENT: 'fs-copy-component',
 };
