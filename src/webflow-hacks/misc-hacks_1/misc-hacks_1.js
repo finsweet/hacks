348ba8df5a2e284e65db62dc0136e582d54c1a12
aexshafii <alexandre.shafii@gmail.com>
2023-01-22 17:10:48 +0300

    fix: fix  remove cookie function

--- src/webflow-hacks/misc-hacks_1/misc-hacks_1.js
+++ src/webflow-hacks/misc-hacks_1/misc-hacks_1.js
@@ -43,5 +43,5 @@ function getCookie(name) {
  * @param name The name of the cookie to remove.
  */
 function removeCookie(name) {
-  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
