5de53b7023dbc208cc757e6587b82568498fe42d
aexshafii <alexandre.shafii@gmail.com>
2023-03-16 14:22:07 +0700

    fix: copy/pasting js from hack 37 was not working, improved the code so it runs properly when copy/pasted

--- src/webflow-hacks/hacks_37/hacks_37.js
+++ src/webflow-hacks/hacks_37/hacks_37.js
@@ -1,18 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
+/**
+ * Open the last tab the user had open when they revisit the page.
+ * @returns {void}
+ */
+function initializeTabs() {
   const STORAGE_KEY = 'previousTabId';
   const tabLinks = document.querySelectorAll('[fs-anchor-element="tab"]');
 
   if (tabLinks.length === 0) return;
-  // add listeners and click chosen tab
   const chosenTabId = localStorage.getItem(STORAGE_KEY);
 
-  tabLinks.forEach(function (tabLink) {
-    const currentTabId = tabLink.id;
+  tabLinks.forEach((tabLink) => {
+    const currentTabId = tabLink.getAttribute('data-w-tab');
 
-    if (chosenTabId === currentTabId) tabLink.click();
+    // Trigger click event on the chosen tab
+    if (chosenTabId === currentTabId) {
+      const tabEvent = new CustomEvent('click');
 
-    tabLink.addEventListener('click', function () {
-      localStorage.setItem(STORAGE_KEY, currentTabId);
+      tabLink.dispatchEvent(tabEvent);
+    }
+
+    // Save the clicked tab to localStorage
+    tabLink.addEventListener('click', () => {
+      if (currentTabId) {
+        localStorage.setItem(STORAGE_KEY, currentTabId);
+      }
     });
   });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Check if Webflow is loaded and use Webflow.push to run the function
+  if (window.Webflow && window.Webflow.push) {
+    window.Webflow.push(initializeTabs);
+  } else {
+    // Fallback to the readystatechange event
+    document.addEventListener('readystatechange', () => {
+      if (document.readyState === 'complete') {
+        initializeTabs();
+      }
+    });
+  }
 });
--- src/webflow-hacks/hacks_37/hacks_37.ts
+++ src/webflow-hacks/hacks_37/hacks_37.ts
@@ -1,17 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const STORAGE_KEY = 'previousTabId';
-  const tabLinks = document.querySelectorAll<HTMLAnchorElement>('[fs-anchor-element="tab"]');
+/**
+ * Open the last tab the user had open when they revisit the page.
+ * @returns {void}
+ */
+function initializeTabs(): void {
+  const STORAGE_KEY: string = 'previousTabId';
+  const tabLinks: NodeListOf<Element> = document.querySelectorAll('[fs-anchor-element="tab"]');
 
   if (tabLinks.length === 0) return;
 
-  // add listeners and click chosen tab
-  const chosenTabId = localStorage.getItem(STORAGE_KEY);
-  tabLinks.forEach(function (tabLink) {
-    const currentTabId = tabLink.id;
-    if (chosenTabId === currentTabId) tabLink.click();
+  const chosenTabId: string | null = localStorage.getItem(STORAGE_KEY);
 
-    tabLink.addEventListener('click', function () {
-      localStorage.setItem(STORAGE_KEY, currentTabId);
+  tabLinks.forEach((tabLink: Element) => {
+    const currentTabId: string | null = tabLink.getAttribute('data-w-tab');
+
+    // Trigger click event on the chosen tab
+    if (chosenTabId === currentTabId) {
+      const tabEvent: CustomEvent = new CustomEvent('click');
+      tabLink.dispatchEvent(tabEvent);
+    }
+
+    // Save the clicked tab to localStorage
+    tabLink.addEventListener('click', () => {
+      if (currentTabId) {
+        localStorage.setItem(STORAGE_KEY, currentTabId);
+      }
     });
   });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Check if Webflow is loaded and use Webflow.push to run the function
+  if (window.Webflow && window.Webflow.push) {
+    window.Webflow.push(initializeTabs);
+  } else {
+    // Fallback to the readystatechange event
+    document.addEventListener('readystatechange', () => {
+      if (document.readyState === 'complete') {
+        initializeTabs();
+      }
+    });
+  }
 });
